b16f9b28e2167f95d72246a627b64d04179d7782
Morteza Ziaeemehr <m.ziaeemehr@gmail.com>
2018-11-21 18:55:43 +0330

    Add currying

---
+++ currying.js
@@ -0,0 +1,16 @@
+// Simple sum
+function sum(x, y) {
+  return x + y
+}
+
+// A sum function using currying
+function sum_with_currying(x) {
+  // Returns a function and gets next arg
+  return y => {
+    // Do the final action
+    return x + y
+  }
+}
+
+console.log(sum(1, 2) // 3
+console.log(sum_with_currying(1)(2)) // 3
